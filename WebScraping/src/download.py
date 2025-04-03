ec2a6639bac44bcf5c23dfdd71fff11d469b2478
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2025-04-03 08:35:27 -0400

    refactor(download): update logic for unique filenames

--- WebScraping/src/download.py
+++ WebScraping/src/download.py
@@ -8,13 +8,16 @@ def download_files(links,folder, base_url):
 
     os.makedirs(folder, exist_ok=True)
 
-    for idx, link in enumerate(links, start=1):
+    for link in links:
         full_url = link if link.startswith("http") else base_url + link
-        path_files = os.path.join(folder, f"Anexo_{idx}.pdf")
-
         response = requests.get(full_url)
 
         if response.status_code == 200 and response.headers.get("Content-Type") == "application/pdf":
+            
+            successful_downloads += 1
+
+            path_files = os.path.join(folder, f"Anexo_{successful_downloads}.pdf")
+
             with open(path_files, "wb") as f:
                 f.write(response.content)
             print(f"Downloaded: {path_files}")
